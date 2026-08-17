de7f68dcf2d102ad69c33b34bcb0701662a3ac94
Augusto528 <65742088+Augusto528@users.noreply.github.com>
2026-08-17 18:21:40 -0300

    teste server 2

--- Backend-telemetria/server.js
+++ Backend-telemetria/server.js
@@ -208,19 +208,40 @@ app.get("/api/celulas", async(req, res) => {
   res.json(data);
 });
 
-// Rota para a coleta do ultimo dado das medicoes individuais das celulas
-app.get("/api/celulas/ultimo", async(req, res) => {
+// // Rota para a coleta do ultimo dado das medicoes individuais das celulas
+// app.get("/api/celulas/ultimo", async(req, res) => {
   
+//   const { data, error } = await supabase
+//     .from("celulas")
+//     .select("*")
+//     .order("id", {ascending: false})
+//     .limit(1);
+
+//   if (error) return res.status(500).json({erro: error.message});
+
+//   res.json(data);
+// });
+
+app.get("/api/celulas/ultimo", async (req, res) => {
+
+  console.log("Consultando tabela celulas...");
+
   const { data, error } = await supabase
     .from("celulas")
     .select("*")
-    .order("id", {ascending: false})
+    .order("id", { ascending: false })
     .limit(1);
 
-  if (error) return res.status(500).json({erro: error.message});
+  console.log("DATA:", data);
+  console.log("ERROR:", error);
+
+  if (error) {
+    return res.status(500).json({
+      erro: error.message
+    });
+  }
 
   res.json(data);
-  res.send(data)
 });
 
 // Para fazer o vercel funcionar
